8b18a8078b4fd0ee8472f0bfd78e926b978388a2
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-05-05 13:22:57 +0200

    Update TypeMatcher.php

--- src/TypeMatcher.php
+++ src/TypeMatcher.php
@@ -4,11 +4,14 @@ namespace Bermuda\Reflection;
 
 final class TypeMatcher
 {
+    /**
+     * @throws \InvalidArgumentException
+     */
     public function match(\ReflectionType $type, mixed $var): bool
     {
         if ($type instanceof \ReflectionNamedType) return $this->matchNamedType($type, $var);
         if ($type instanceof \ReflectionUnionType) return $this->matchUnionType($type, $var);
-        if ($type instanceof \ReflectionIntersectionType) return $this->matchIntersectionType($type, $var);
+        if ($type instanceof \ReflectionIntersectionType) return $this->matchIntersectionType($type);
 
         throw new \InvalidArgumentException(sprintf('%s type not supported', $type::class));
     }
@@ -16,15 +19,35 @@ final class TypeMatcher
     private function matchNamedType(\ReflectionNamedType $type, mixed $var): bool
     {
         if ($type->allowsNull() && $var === null) return true;
-        if ($type->isBuiltin()) return ('is_'.$type->getName())($var);
+        if ($type->isBuiltin()) return $this->matchBuiltinType($type, $var);
 
         return $this->isInstanceOf($var, $type->getName());
+    }
+
+    private function matchBuiltinType(\ReflectionType $type, mixed $var): bool
+    {
+        return match ($type->getName()) {
+            'mixed' => true,
+            'object' => is_object($var),
+            'array' => is_array($var),
+            'bool' => is_bool($var),
+            'int' => is_int($var),
+            'float' => is_float($var),
+            'string' => is_string($var),
+            'false' => $var === false,
+            'true' => $var === true,
+            'callable' => is_callable($var),
+            'iterable' => is_iterable($var),
+            default => false
+        };
     }
 
     private function matchUnionType(\ReflectionUnionType $type, mixed $var): bool
     {
         if ($type->allowsNull() && $var === null) return true;
-        foreach ($type->getTypes() as $type) if ($this->match($type, $var)) return true;
+        foreach ($type->getTypes() as $type) {
+            if ($this->match($type, $var)) return true;
+        }
 
         return false;
     }
@@ -32,11 +55,13 @@ final class TypeMatcher
     private function matchIntersectionType(\ReflectionIntersectionType $type, mixed $var): bool
     {
         if ($type->allowsNull() && $var === null) return true;
-        foreach ($type->getTypes() as $type) if (!$this->match($type, $var)) return false;
+        foreach ($type->getTypes() as $type) {
+            if (!$this->match($type, $var)) return false;
+        }
 
         return true;
     }
-    
+
     private function isInstanceOf(mixed $obj, string $cls): bool
     {
         return $obj instanceof $cls;
